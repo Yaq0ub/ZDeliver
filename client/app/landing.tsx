7093cb0e67bf859c6e9222ca82d095a24819b0a9
Yaq0ub <122842387+Yaq0ub@users.noreply.github.com>
2024-02-09 18:42:21 -0700

    Client/component: Added landing page component

---
+++ client/app/landing.tsx
@@ -0,0 +1,24 @@
+import { Button, StyleSheet } from "react-native";
+import { router } from "expo-router";
+
+import { Text, View } from "../components/Themed";
+
+export default function LandingScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Landing Screen</Text>
+      <Button title={"Login"} onPress={() => router.push("/login")} />
+      <Button title={"Register"} onPress={() => router.push("/register")} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+  },
+});
