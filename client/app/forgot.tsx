fce34b5413504db805b34500ef8a61e029de7a2b
Yaq0ub <122842387+Yaq0ub@users.noreply.github.com>
2024-02-15 12:31:31 -0700

    Client/Styles: added seperate styles file for forgot page

--- client/app/forgot.tsx
+++ client/app/forgot.tsx
@@ -1,6 +1,8 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+// Import styles
+import styles from '../styles/forgot.styles'
 const forgot = () => {
   return (
     <View>
