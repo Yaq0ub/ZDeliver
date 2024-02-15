1bb2e2a015859e1fc1cf8ff76e014285a0edf3ac
Yaq0ub <122842387+Yaq0ub@users.noreply.github.com>
2024-02-14 23:14:55 -0700

    Client/Services: update readProductInventory to cache Fetched list and only call it once a day

--- client/services/other/readProductsInventory.ts
+++ client/services/other/readProductsInventory.ts
@@ -1,22 +1,38 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { db } from "../../firebase/firebaseConfig";
 import { collection, getDocs } from 'firebase/firestore';
 
 const readProductsInventory = async () => {
+  const currentTime = new Date().getTime();
+  const lastFetchTime = await AsyncStorage.getItem('lastFetchTime');
+  const oneDay = 24 * 60 * 60 * 1000; // Milliseconds in one day
+
+  // Check if we need to fetch new data or use cached data
+  if (lastFetchTime && currentTime - parseInt(lastFetchTime) < oneDay) {
+    const cachedData = await AsyncStorage.getItem('productsInventory');
+    if (cachedData) return JSON.parse(cachedData);
+  }
+
+  // Proceed with Firestore call if more than a day has passed or no cache exists
   const productsCollectionRef = collection(db, "ProductsInventory");
   try {
     const querySnapshot = await getDocs(productsCollectionRef);
     const productsDictionary: { [key: string]: { name: string; category: string; price: number; uri: string; } } = {};
 
     querySnapshot.forEach((doc) => {
-      // Assuming doc.id is the key
       productsDictionary[doc.id] = doc.data() as { name: string; category: string; price: number; uri: string; };
     });
 
-    console.log(productsDictionary);
+    // Update cache and last fetch timestamp
+    await AsyncStorage.setItem('productsInventory', JSON.stringify(productsDictionary));
+    await AsyncStorage.setItem('lastFetchTime', currentTime.toString());
+
     return productsDictionary;
   } catch (error) {
     console.error("Error reading documents: ", error);
-    return {};
+    // Optionally, return cached data in case of an error
+    const cachedData = await AsyncStorage.getItem('productsInventory');
+    return cachedData ? JSON.parse(cachedData) : {};
   }
 };
 
