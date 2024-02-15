2198db2061188765f41f82987e0cd6e94c9cd8f9
Yaq0ub <122842387+Yaq0ub@users.noreply.github.com>
2024-02-14 17:08:57 -0700

    Client/Component: Added placed order component

---
+++ client/app/placed.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const placed = () => {
+  return (
+    <View>
+      <Text>placed</Text>
+    </View>
+  )
+}
+
+export default placed
