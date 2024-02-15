f38cad9e8ae9fcb3fb6c8d0108306c9066dd8e6a
Yaq0ub <122842387+Yaq0ub@users.noreply.github.com>
2024-02-14 17:09:53 -0700

    Client/Constants: Added account settings routes as constants

---
+++ client/constants/consts.ts
@@ -0,0 +1,12 @@
+const ACCOUNT_ROUTES = [
+    { label: 'Manage account', route: "manage" },
+    { label: 'Orders', route: "orders" },
+    { label: 'Payments', route: "payments" },
+    { label: 'Addresses', route: "addresses" },
+    { label: 'Privacy', route: "privacy" },
+    { label: 'Notifications', route: "notifications" },
+    { label: 'Logout', route: "logout" },];
+
+export {
+    ACCOUNT_ROUTES,
+}
