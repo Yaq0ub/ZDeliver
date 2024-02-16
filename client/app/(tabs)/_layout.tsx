77519389e020f6efe785c55f62e92352585a9ad6
Yaq0ub <122842387+Yaq0ub@users.noreply.github.com>
2024-02-15 23:19:56 -0700

    client/Layout-codestructure: change code tabs _layout.tsx code structure

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -1,15 +1,12 @@
 import React, { useState } from "react"
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
-import { Pressable, Text, useColorScheme } from 'react-native';
+import { Pressable, Text} from 'react-native';
 import { router } from 'expo-router';
 
 import Colors from '../../constants/Colors';
 
-//import { useColorScheme } from "@/components/useColorScheme";
-//import { useClientOnlyValue } from "@/components/useClientOnlyValue";
 import { auth } from "../../firebase/firebaseConfig";
-import { StatusBar } from "expo-status-bar";
 import { Ionicons } from "@expo/vector-icons";
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
@@ -22,24 +19,8 @@ function TabBarIcon(props: {
 }
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
   const [isLoading, setIsLoading] = useState(true);
-  const BackButton = () => {
-    return (
-      <Link href="/(tabs)/account" asChild>
-        <Pressable>
-          {({ pressed }) => (
-            <Ionicons
-              name="chevron-back"
-              size={25}
-              color={Colors.light}
-              style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-            />
-          )}
-        </Pressable>
-      </Link>
-    )
-  }
+  
   // Show landing screen if logged out
   auth.onAuthStateChanged((user: any) => {
     setIsLoading(false);
@@ -89,80 +70,20 @@ export default function TabLayout() {
 
         }}
       />
-      <Tabs.Screen
-        name="account"
+      <Tabs.Screen 
+        name="account" 
         options={{
           title: 'Account',
           tabBarIcon: ({ color }) => <TabBarIcon name="gear" color={color} />,
         }}
       />
-
-      <Tabs.Screen
-        name="addresses"
-        options={{
-          href: null,
-          headerTitle: 'Addresses',
-          headerTitleAlign: 'center',
-          headerLeft: () => (<BackButton />),
-        }}
-      />
-      
-      <Tabs.Screen
-        name="logout"
-        options={{
-          href: null,
-          headerTitle: 'Logout',
-          headerTitleAlign: 'center',
-          headerLeft: () => (<BackButton />),
-          headerStyle:{backgroundColor:Colors.primary},
-          headerTintColor: Colors.light
-        }}
-      />
-      <Tabs.Screen
-        name="manage"
-        options={{
-          href: null,
-          headerTitle: 'Manage',
-          headerTitleAlign: 'center',
-          headerLeft: () => (<BackButton />),
-        }}
-      />
-      <Tabs.Screen
-        name="notifications"
-        options={{
-          href: null,
-          headerTitle: 'Notifications',
-          headerTitleAlign: 'center',
-          headerLeft: () => (<BackButton />)
-        }}
-      />
-      <Tabs.Screen
-        name="orders"
-        options={{
-          href: null,
-          headerTitle: 'Orders',
-          headerTitleAlign: 'center',
-          headerLeft: () => (<BackButton />)
-        }}
-      />
-      <Tabs.Screen
-        name="payments"
-        options={{
-          href: null,
-          headerTitle: 'Payments',
-          headerTitleAlign: 'center',
-          headerLeft: () => (<BackButton />)
-        }}
-      />
-      <Tabs.Screen
-        name="privacy"
-        options={{
-          href: null,
-          headerTitle: 'Privacy',
-          headerTitleAlign: 'center',
-          headerLeft: () => (<BackButton />)
-        }}
-      />
+      <Tabs.Screen name="addresses"options={{href: null,headerTitle: 'Addresses',}}/>
+      <Tabs.Screen name="logout" options={{href: null, headerTitle: 'Logout'}}/>
+      <Tabs.Screen name="manage" options={{href: null, headerTitle: 'Manage',}}/>
+      <Tabs.Screen name="notifications" options={{ href: null, headerTitle: 'Notifications',}}/>
+      <Tabs.Screen name="orders" options={{ href: null,headerTitle: 'Orders',}}/>
+      <Tabs.Screen name="payments"options={{ href: null,headerTitle: 'Payments',}}/>
+      <Tabs.Screen name="privacy" options={{ href: null,headerTitle: 'Privacy',}}/>
     </Tabs>
   );
 }
