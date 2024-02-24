2d798e78d34fc25686592fd3cc7258c4ad8a2250
Yaq0ub <122842387+Yaq0ub@users.noreply.github.com>
2024-02-24 05:20:27 -0700

    client/refactor-productsfilter: extracted styles to separate file for better modularity

--- client/redux/features/products/ProductsFilter.tsx
+++ client/redux/features/products/ProductsFilter.tsx
@@ -1,8 +1,7 @@
 // FilterPicker.tsx
 import React from "react";
-import { StyleSheet, ScrollView, TouchableOpacity, Text } from "react-native";
-import Colors from "../../../constants/Colors";
-
+import {ScrollView, TouchableOpacity, Text } from "react-native";
+import styles from '../../../styles/components/ProductsFilter.styles'
 import {useAppSelector, useAppDispatch } from '../../hooks';
 
 import { 
@@ -35,31 +34,5 @@ const ProductsFilter: React.FC = () => {
     </ScrollView>
   );
 };
-
-// Styles remain unchanged
-
-const styles = StyleSheet.create({
-  filterContainer: {
-    flexDirection: "row",
-    alignItems: 'center', // Align items in the center vertically
-    paddingHorizontal: 10,
-  },
-  filterField: {
-    backgroundColor: "#e0e0e0",
-    borderRadius: 20,
-    paddingHorizontal: 15,
-    paddingVertical: 4,
-    marginRight: 10,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  selected: {
-    backgroundColor: Colors.lightBlue,
-  },
-  filterText: {
-    color: "#000",
-    fontSize: 12
-  },
-});
 
 export default ProductsFilter
