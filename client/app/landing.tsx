39a22f3dc8d71b99a6cb0bb1a197923629853910
Yaq0ub <122842387+Yaq0ub@users.noreply.github.com>
2024-02-24 05:10:43 -0700

    client/feature-landingpage: implemented sign-out and clear AsyncStorage on landing

--- client/app/landing.tsx
+++ client/app/landing.tsx
@@ -1,20 +1,40 @@
 // Import necessary React Native components
-import React from 'react'; // Import React
+import React, { useEffect } from 'react'; // Import React
 import {View, Text, TouchableOpacity, Image} from 'react-native';
 import { router } from 'expo-router'; // Import router for navigation
 import { StatusBar } from 'expo-status-bar';
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import storage from '@react-native-async-storage/async-storage';
 // Import styles
 import styles from '../styles/landing.styles'
+import { signOut, getAuth } from 'firebase/auth';
+import { useDispatch } from 'react-redux';
+import { clearProductsState } from '../redux/features/products/productsSlice';
 
 // LandingScreen component definition
 export default function LandingScreen() {
-  // Function to navigate to the login screen
   const loginRoute = () => router.push('/login');
-
-  // Function to navigate to the registration screen
   const registerRoute = () => router.push('/register');
 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const clearStorage = async () => {
+      try {
+        signOut(getAuth())
+        dispatch(clearProductsState())
+        await AsyncStorage.clear();
+        AsyncStorage.removeItem('persist:root')
+        await storage.clear();
+        console.log('Storage successfully cleared!');
+      } catch (e) {
+        console.log('Failed to clear the async storage.');
+      }
+    };
+
+    clearStorage();
+  }, []);
   return (
     <View style={styles.container}>
       <StatusBar style="dark"/>
@@ -31,12 +51,9 @@ export default function LandingScreen() {
         {/* Login button */}
         <TouchableOpacity onPress={loginRoute}>
         <View style={styles.button}>
-          
             <Text style={styles.buttonText}>Login</Text>
-          
         </View>
         </TouchableOpacity>
-
         {/* Register button */}
         <TouchableOpacity onPress={registerRoute}>
         <View style={styles.button}>
