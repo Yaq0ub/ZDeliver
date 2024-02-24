cdf4337203b4e114915cb97bdec5c27ee9023eb3
Yaq0ub <122842387+Yaq0ub@users.noreply.github.com>
2024-02-24 14:24:25 -0700

    client/type-enhancements: added OrderType, PaymentType, and AddressType definitions

--- client/constants/types.ts
+++ client/constants/types.ts
@@ -6,7 +6,30 @@ export type ProductItemType = {
     uri: string;
     count: number;
 };
+
+export type OrderType = {
+    id: string;
+    items: ProductItemType[];
+    method: string;
+    status: 'Delivered' | 'Pickedup' | 'Placed' | 'Confirmed' | 'Cancelled';
+    address: string;
+    Total: number;
+    paymentid: string;
+}
+export type PaymentType = {
+    id: string;
+    name: string;
+    last4: string;
+}
+export type AddressType = {
+    id: string;
+    area: string
+    street1: string;
+    street2: string;
+    zipcode: string;
+}
 export interface ProductListProps {
     selectedFilter: string;
     productsInventory: any;
-  }
+}
+
