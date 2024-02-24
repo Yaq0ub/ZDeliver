7e4fd98a45357b49340a20570a8eb848bd666807
Yaq0ub <122842387+Yaq0ub@users.noreply.github.com>
2024-02-24 05:19:04 -0700

    client/optimization-cart: removed array conversion in cartList for direct item type compatibility

--- client/redux/features/products/CartList.tsx
+++ client/redux/features/products/CartList.tsx
@@ -1,49 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { FlatList } from 'react-native';
+import ProductCard from './ProductCard';
+import styles from '../../../styles/components/ProductList.styles';
 
-import ProductCard from '../../../components/ProductCard';
-
-// Import Styles
-import styles from '../../../styles/components/ProductList.styles'
-
-// Import types
-import { ProductItemType } from '../../../constants/types'
+import { ProductItemType } from '../../../constants/types';
 
 import { useAppSelector } from '../../hooks';
 
 const CartList: React.FC = () => {
-
-    // State to hold the array of products
-    const cartList = useAppSelector((state) => state.products.cartList);
-
-    const [cartItemsArray, setCartItemsArray] = useState<any>()
-
+    const cartList = useAppSelector((state) => state.products.cart);
     useEffect(() => {
-        // Convert the object into an array suitable for FlatList
-
-        const cartArray = Object.keys(cartList).map(key => ({
-            ...cartList[key],
-            key, // Assuming each product doesn't already include a 'key' property
-          }));
-          
-        setCartItemsArray(cartArray)
-    }, [cartList]); // Re-fetch or filter products when selectedFilter changes
+       
+    }, [cartList]); // `dispatch` is stable and doesn't change, so it's safe to include it here
 
     // Render function for each ProductCard
     const renderProduct = ({ item }: { item: ProductItemType }) => (
-        <ProductCard productID={item.key}/>
+        <ProductCard item={item} />
     );
 
     return (
         <FlatList
-            data={cartItemsArray}
+            data={cartList}
             renderItem={renderProduct}
-            keyExtractor={(item) => item.key}
+            keyExtractor={(item, index) => item.id || String(index)}
             style={styles.container}
         />
     );
 };
-
-
 
 export default CartList;
