62f736c6bf7cbe2bcddfe131a000ebe8659baebd
Yaq0ub <122842387+Yaq0ub@users.noreply.github.com>
2024-02-26 16:45:27 -0700

    Client/DBListeners: Added cart and address list listeners

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -4,27 +4,32 @@ import ProductsFilter from "../../redux/features/products/ProductsFilter";
 import ProductsList from "../../redux/features/products/ProductsList";
 import styles from '../../styles/home.styles';
 import { StatusBar } from "expo-status-bar";
-import { useAppDispatch } from "../../redux/hooks";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { getProductsAsync} from "../../redux/features/products/productsSlice";
 
-import { setupCartAndSubtotalListeners } from "../../services/firebase/setupCartAndSubtotalListeners";
+import { setupCartListeners } from "../../services/firestore/setupCartListener";
+import { setupAddressesListeners } from "../../services/firestore/setupAddressesListener";
 
 export default function HomeScreen() {
   
   const dispatch = useAppDispatch();
+  const userAuthenticated = useAppSelector(state => state.auth.isAuthenticated)
 
   useEffect(() => { 
-    const fetchProducts = async () => {
-      try {
-        dispatch(getProductsAsync());
-      } catch (error) {
-        console.error("Failed to fetch products", error);
-      }
-    };
+    if (userAuthenticated) {
+      const fetchProducts = async () => {
+        try {
+          dispatch(getProductsAsync());
+        } catch (error) {
+          console.error("Failed to fetch products", error);
+        }
+      };
 
-    fetchProducts();
-    setupCartAndSubtotalListeners(dispatch);
-  }, []);
+      fetchProducts();
+      setupCartListeners(dispatch);
+      setupAddressesListeners(dispatch)
+    }
+  }, [userAuthenticated]); 
   
   return (
     <View style={styles.container}>
