712da8d1d4c75cb924542d4f2234cc0ee0b38be9
Yaq0ub <122842387+Yaq0ub@users.noreply.github.com>
2024-02-26 16:55:55 -0700

    client/comment-productsSlice: add comments

--- client/redux/features/products/productsSlice.ts
+++ client/redux/features/products/productsSlice.ts
@@ -1,22 +1,26 @@
-
+// Imports from Redux Toolkit and local files for functionality and type definitions
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../../store';
-
 import { ProductItemType } from '../../../constants/types';
-import { addToUserCart } from '../../../services/firebase/addToUserCart';
-import { removeFromUserCart } from '../../../services/firebase/removeFromUserCart';
-import { calculateUserCartSubtotal } from '../../../services/firebase/calculateUserCartSubtotal';
-import { fetchProductsInventory } from '../../../services/firebase/fetchProductsInventory';
 
+// Importing CRUD operations from services
+import { addToUserCart } from '../../../services/firestore/addToUserCart';
+import { removeFromUserCart } from '../../../services/firestore/removeFromUserCart';
+import { fetchProductsInventory } from '../../../services/firestore/fetchProductsInventory';
+
+/**
+ * Type definition for the state managed by this slice.
+ */
 interface ProductsState {
   categories: string[];
   products: ProductItemType[];
   selectedCategory: string;
-  cart: ProductItemType[]
+  cart: ProductItemType[];
   subtotal: number;
   fees: number;
 }
 
+// The initial state for the products slice, setting up default values.
 const initialState: ProductsState = {
   categories: ['All'],
   products: [],
@@ -26,104 +30,97 @@ const initialState: ProductsState = {
   fees: 0,
 };
 
+/**
+ * Async thunk for fetching products from the inventory.
+ * @returns {Promise<any>} The fetched products and categories.
+ */
 export const getProductsAsync = createAsyncThunk<any>(
   'products/fetchProducts',
-  async (_,) => {
+  async () => {
     try {
       const cacheData = await fetchProductsInventory();
       return cacheData;
     } catch (error) {
       console.error("Error fetching products inventory: ", error);
-      // Return a default value or handle the error as appropriate for your app
       return { categoriesList: [], productsList: [] };
     }
   }
 );
 
-export const addToCartAsync = createAsyncThunk<void, ProductItemType, { state: RootState }>(
-  'cart/addToCartAsync',
-  async (item, { dispatch }) => {
-    // Directly use writeToUserCart without passing userId
-    await addToUserCart(item);
+/**
+ * Async thunk for adding an item to the user's cart.
+ * @param {ProductItemType} item The item to be added to the cart.
+ * @param {RootState} state The current state of the Redux store.
+ */
+export const addToCartAsync = createAsyncThunk<
+  void,
+  ProductItemType,
+  { state: RootState }>(
+    'cart/addToCartAsync',
+    async (item) => {
+      await addToUserCart(item);
+    }
+  );
 
-    // Call calculateSubtotalAsync to update subtotal after adding item
-    dispatch(calculateSubtotalAsync());
-  }
-);
+/**
+ * Async thunk for removing an item from the user's cart.
+ * @param {ProductItemType} item The item to be removed from the cart.
+ */
+export const removeFromCartAsync = createAsyncThunk<
+  void,
+  ProductItemType,
+  { state: RootState }>(
+    'cart/removeFromCartAsync',
+    async (item) => {
+      await removeFromUserCart(item);
+    }
+  );
 
-export const removeFromCartAsync = createAsyncThunk<void, ProductItemType, { state: RootState }>(
-  'cart/removeFromCartAsync',
-  async (item, { dispatch }) => {
-    await removeFromUserCart(item);
-
-    // Call calculateSubtotalAsync to update subtotal after removing item
-    dispatch(calculateSubtotalAsync());
-  }
-);
-
-export const calculateSubtotalAsync = createAsyncThunk<void, void, { state: RootState }>(
-  'cart/calculateSubtotalAsync',
-  async (_,) => {
-    await calculateUserCartSubtotal();
-  }
-);
-
+// The slice for products, containing reducers and extra reducers for handling async operations.
 export const productsSlice = createSlice({
   name: 'products',
   initialState,
   reducers: {
+    /**
+     * Sets the selected category in the state.
+     * @param {PayloadAction<string>} action The action payload containing the category.
+     */
     setSelectedCategory: (state, action: PayloadAction<string>) => {
-      state.selectedCategory = action.payload
+      state.selectedCategory = action.payload;
     },
-    // Updates the cart list with the new data
+    /**
+     * Updates the cart with new data and recalculates product counts.
+     * @param {PayloadAction<ProductItemType[]>} action The action payload containing the new cart data.
+     */
     updateCart: (state, action: PayloadAction<ProductItemType[]>) => {
       const updatedCart = action.payload;
       state.cart = updatedCart;
-      // Directly modify the products array to update counts based on the cart
-      state.products = state.products.map(product => {
-        // Find the product in the updated cart
-        const cartItem = updatedCart.find(cartItem => cartItem.id === product.id);
-        
-        // If the product is found in the cart, update its count, otherwise set to 0
-        return {
-          ...product,
-          count: cartItem ? cartItem.count : 0,
-        };
+      state.products.forEach((product) => {
+        const cartItem = updatedCart.find((item) => item.id === product.id);
+        product.count = cartItem ? cartItem.count : 0;
       });
+      // CalculateSubtotal
+      state.subtotal = state.cart.reduce((subtotal, item) => subtotal + (item.price * item.count), 0);
     },
-    updateSubtotal: (state, action: PayloadAction<number>) => {
-      state.subtotal = action.payload;
-    },
-    clearProductsState: () => {
-      return initialState;
-    },
+    /**
+     * Resets the products state to its initial values.
+     */
+    clearProductsState: () => initialState,
   },
-
   extraReducers: (builder) => {
     builder.addCase(getProductsAsync.fulfilled, (state, action) => {
       const { categoriesList, productsList } = action.payload;
       state.categories = categoriesList;
       state.products = productsList;
-      state.products = state.products.map(product => {
-        // Find the product in the updated cart
-        const cartItem = state.cart.find(cartItem => cartItem.id === product.id);
-        
-        // If the product is found in the cart, update its count, otherwise set to 0
-        return {
-          ...product,
-          count: cartItem ? cartItem.count : 0,
-        };
+      state.products.forEach((product) => {
+        const cartItem = state.cart.find((item) => item.id === product.id);
+        product.count = cartItem ? cartItem.count : 0;
       });
+      
     });
-  }
-
+  },
 });
 
-export const { 
-  setSelectedCategory,
-  updateCart,
-  updateSubtotal, 
-  clearProductsState } = productsSlice.actions;
-
-//export const selectProductsList = (state: RootState) => state.products.list
+// Exporting the slice's actions and the reducer itself.
+export const { setSelectedCategory, updateCart, clearProductsState } = productsSlice.actions;
 export default productsSlice.reducer;
